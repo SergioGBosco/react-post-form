1d4ab2c625a756558e72fbd731e79c4f6007fa83
SergioGBosco <sergiogibosco@gmail.com>
2025-09-03 16:56:08 +0200

    feat: add a fuction for change and click

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import axios from "axios"
+import { Alert } from "bootstrap"
 import { useState } from "react"
 function App() {
 
   // axios.get("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts").then(resp => {
   //   console.log(resp.data)
   // })
+
+
+  //creo la funzione che mi permette di inviare i dati dei campi imput
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
+
+
+  //creo una funzione per gestire l'invio del pulsante
+  const handleClick = (e) => {
+    e.preventDefault();
+    alert("Post Aggiunto con Successo");
+  }
 
   return (
     <div className="container">
@@ -13,21 +27,48 @@ function App() {
           <form>
             <div className="mb-3">
               <label className="form-label">Autore</label>
-              <input type="text" className="form-control" id="InputAuthor" />
+              <input
+                type="text"
+                className="form-control"
+                id="InputAuthor"
+                name="author"
+                onChange={handleChange}
+              />
             </div>
             <div className="mb-3">
               <label className="form-label">Titolo</label>
-              <input type="text" className="form-control" id="InputTitle" />
+              <input
+                type="text"
+                className="form-control"
+                id="InputTitle"
+                name="title"
+                onChange={handleChange}
+              />
+
+
             </div>
             <div className="mb-3">
               <label className="form-label">Testo</label>
-              <input type="text" className="form-control" id="InputText" />
+              <input
+                type="text"
+                className="form-control"
+                id="InputText"
+                name="text"
+                onChange={handleChange}
+              />
             </div>
+
             <div className="mb-3 form-check">
-              <input type="checkbox" className="form-check-input" id="checkPublic" />
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="checkPublic"
+                name="public"
+                onChange={handleChange}
+              />
               <label className="form-check-label">Rendi Pubblico</label>
             </div>
-            <button type="submit" className="btn btn-primary">Invia</button>
+            <button type="submit" className="btn btn-primary" onClick={handleClick}>Invia</button>
           </form>
         </div>
       </div>
